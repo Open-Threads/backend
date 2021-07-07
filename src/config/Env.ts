e975a20cfe8b0fe973711d0282af22c0c4b02fc4
Raisess <raisechif@gmail.com>
2021-07-07 09:50:07 -0300

    prettier

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -2,5 +2,6 @@ import "dotenv/config";
 
 export default class Env {
   public static PORT: number = Number(process.env.PORT) || 8080;
-  public static MONGO_URL: string = process.env.MONGO_URL || "mongodb://localhost/open-threads"
+  public static MONGO_URL: string =
+    process.env.MONGO_URL || "mongodb://localhost/open-threads";
 }
