b8918287e230bc0275913fea595b147661cf60dd
Raisess <raisechif@gmail.com>
2021-07-11 11:28:18 -0300

    updated env vars

--- src/config/Env.ts
+++ src/config/Env.ts
@@ -4,4 +4,6 @@ export default class Env {
   public static PORT: number = Number(process.env.PORT) || 8080;
   public static MONGO_URL: string =
     process.env.MONGO_URL || "mongodb://localhost/open-threads";
+  public static TOKEN_SECRET: string = process.env.TOKEN_SECRET!;
+  public static TOKEN_DURATION: string = process.env.TOKEN_DURATION || "7h";
 }
